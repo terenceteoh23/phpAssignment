de0b5de1e428d9179d6d435d6ad338bde5c1027c
Teohjh <67089669+Teohjh@users.noreply.github.com>
2020-06-22 22:22:59 +0800

    Add files via upload

---
+++ footer1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style1.css"/>
+	</head>
+	<body>
+		<!--Start Footer-->
+		<footer class="site-footer">
+			<div class="container">
+				<div class="row-container">
+					<div class="colm-container">
+						<h6>About</h6>
+						<p class="text-justify">About.</p>
+					</div>
+					
+					<div class="colm-container">
+						<h6>Categories</h6>
+						<ul class="footer-links">
+						  <li></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!--End Footer-->
+	</body>
+</html>
