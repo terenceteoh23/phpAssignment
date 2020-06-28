a9159c3bf1d5a70ea8981a48665bd05ac1c9de84
terenceteoh23 <67060092+terenceteoh23@users.noreply.github.com>
2020-06-28 23:38:52 +0800

    Create Template.php

---
+++ navibar and footer/Template.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="general.css">
+	<title>About Us</title>
+</head>
+<body>
+	<div class="banner">
+		<img src="images/banner2.jpg" alt="banner">
+	</div>
+	<div class="navibar">
+		<a class="active" href="#">Home</a>
+		<a href="#">About</a>
+		<a href="#">FAQ</a>
+		<a href="#">Tickets</a>
+		<a href="#" style="float: right;">Log in</a>
+		<a href="#" style="float: right;">Sign up</a>
+	</div>
+
+	<p>something something</p>
+
+	<footer>
+		<div class="box">
+			<ul>
+				<li><h3>Navigation</h3></li>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">FAQ</a></li>
+				<li><a href="#">Tickets</a></li>
+				<li><a href="#">Log in</a></li>
+				<li><a href="#">Sign up</a></li>
+			</ul>
+		</div>
+		<div class="box">
+			<p>Insert description</p>
+		</div>
+		<div class="box">
+			<p>Insert description</p>
+		</div>
+	</footer>
+</body>
+</html>
